1822dbed5156c373157ced3f7caef1726fd9f1a5
Yusent Chig <yusent@protonmail.com>
2020-07-31 16:03:04 -0700

    Fix typo: Remove 'is' prefix from spec descriptions

--- test/Model/PlayerSpec.hs
+++ test/Model/PlayerSpec.hs
@@ -10,15 +10,15 @@ import Model.Player
 spec :: Spec
 spec = do
   describe "playerGoalsQuota" $ do
-    it "is should return the player's level quota" $ property $
+    it "should return the player's level quota" $ property $
       \p -> playerGoalsQuota p == playerLevelGoalsQuota (playerLevel p)
 
   describe "addPlayerToTeam" $ do
-    it "is should add player to the beginning of the player's list" $ property $
+    it "should add player to the beginning of the player's list" $ property $
       \p t -> head (teamPlayers $ addPlayerToTeam p t) == p
 
-    it "is should increment team's goals count by the player's goals count" $ property $
+    it "should increment team's goals count by the player's goals count" $ property $
       \p t -> teamGoalsCount (addPlayerToTeam p t) - playerGoalsCount p == teamGoalsCount t
 
-    it "is should increment team's goals quota by the player's goals quota" $ property $
+    it "should increment team's goals quota by the player's goals quota" $ property $
       \p t -> teamGoalsQuota (addPlayerToTeam p t) - playerGoalsQuota p == teamGoalsQuota t
