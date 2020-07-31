f0410134ec4015ab08658134bb1d7611babae06d
Yusent Chig <yusent@protonmail.com>
2020-07-31 14:36:43 -0700

    Add Team data type and a function to group players by team

--- src/Model/Player.hs
+++ src/Model/Player.hs
@@ -3,15 +3,22 @@ module Model.Player where
 import Import (Text)
 
 data Player = Player
-    { name :: Text
+    { playerName :: Text
     , playerLevel :: PlayerLevel
-    , goalsCount :: Int
-    , salary :: Rational
-    , bonus :: Rational
-    , team :: Text
+    , playerGoalsCount :: Int
+    , playerSalary :: Rational
+    , playerBonus :: Rational
+    , playerTeamName :: Text
     }
 
 data PlayerLevel = A | B | C | Cuauh
+
+data Team = Team
+    { teamName :: Text
+    , teamPlayers :: [Player]
+    , teamGoalsCount :: Int
+    , teamGoalsQuota :: Int
+    }
 
 playerGoalsQuota :: Player -> Int
 playerGoalsQuota = playerLevelGoalsQuota . playerLevel
@@ -21,3 +28,31 @@ playerLevelGoalsQuota A = 5
 playerLevelGoalsQuota B = 10
 playerLevelGoalsQuota C = 15
 playerLevelGoalsQuota Cuauh = 20
+
+groupPlayersIntoTeams :: [Player] -> [Team]
+groupPlayersIntoTeams = groupPlayersIntoTeamsRec []
+
+groupPlayersIntoTeamsRec :: [Team] -> [Player] -> [Team]
+groupPlayersIntoTeamsRec teams [] = teams
+groupPlayersIntoTeamsRec teams (player : restOfPlayers) = groupPlayersIntoTeamsRec updatedTeams restOfPlayers
+  where
+    updatedTeams = insertPlayer teams
+    insertPlayer [] = [newTeamFromPlayer player]
+    insertPlayer (team : restOfTeams)
+      | teamName team == playerTeamName player = addPlayerToTeam player team : restOfTeams
+      | otherwise = team : insertPlayer restOfTeams
+
+newTeamFromPlayer :: Player -> Team
+newTeamFromPlayer player = Team
+  { teamName = playerTeamName player
+  , teamPlayers = [player]
+  , teamGoalsCount = playerGoalsCount player
+  , teamGoalsQuota = playerGoalsQuota player
+  }
+
+addPlayerToTeam :: Player -> Team -> Team
+addPlayerToTeam player team = team
+  { teamPlayers = player : teamPlayers team
+  , teamGoalsCount = playerGoalsCount player + teamGoalsCount team
+  , teamGoalsQuota = playerGoalsQuota player + teamGoalsQuota team
+  }
