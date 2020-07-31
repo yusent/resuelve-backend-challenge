44f31ca4178c98053d4b3c11301a288fe1bf90d1
Yusent Chig <yusent@protonmail.com>
2020-07-31 16:03:19 -0700

    Add newTeamFromPlayer test

--- test/Model/PlayerSpec.hs
+++ test/Model/PlayerSpec.hs
@@ -4,7 +4,7 @@ module Model.PlayerSpec (spec) where
 
 import Prelude (head)
 import TestImport hiding (head)
-import Test.QuickCheck (property)
+import Test.QuickCheck (conjoin, property)
 import Model.Player
 
 spec :: Spec
@@ -12,6 +12,16 @@ spec = do
   describe "playerGoalsQuota" $ do
     it "should return the player's level quota" $ property $
       \p -> playerGoalsQuota p == playerLevelGoalsQuota (playerLevel p)
+
+  describe "newTeamFromPlayer" $ do
+    it "should produce a team with the same name as the given player's teamName" $ property $
+      \p -> teamName (newTeamFromPlayer p) == playerTeamName p
+
+    it "should produce a team containing only the given player" $ conjoin
+      [ property $ \p -> teamPlayers (newTeamFromPlayer p) == [p]
+      , property $ \p -> teamGoalsCount (newTeamFromPlayer p) == playerGoalsCount p
+      , property $ \p -> teamGoalsQuota (newTeamFromPlayer p) == playerGoalsQuota p
+      ]
 
   describe "addPlayerToTeam" $ do
     it "should add player to the beginning of the player's list" $ property $
